ebd65cd2c48492ca6e4fc9ad6741100bd8f7f29d
Denis Lochmelis <dlochmelis@gmail.com>
2023-05-28 15:37:23 +0200

    Prepare merge into development

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -5,21 +5,12 @@ import kotlin.time.Duration.Companion.seconds
 fun main() {
     val runner = WorkerTestRunner
 
-//    val parameters = variateParameters(
-//        getAffinityManager()?.scheduleShort2().orUnrestricted(2), // affinityScheduleUnrestricted(2),
-//        generateSequence(2) { it * 3 }.take(5).toList(),
-//        listOf(null /* { MemShuffler(50_000) } */),
-//        listOf(::CinteropBarrier)
-//    ).toList()
     val parameters = variateParameters(
         listOf(null),
         generateSequence(3) { it * 3 }.take(5).toList(),
         listOf(null),
         listOf(::CinteropBarrier)
     ).toList()
-    // TODO: on x86 manually run tests with some affinity
-    // TODO: on macos also run without parallel (just in case they interfere too much)
-    // TODO: also run release AND debug
     val singleTestDuration = 3.seconds
 
     val tests = listOf(
@@ -58,7 +49,7 @@ fun main() {
         val forbiddenCount = results.countOfType(OutcomeType.FORBIDDEN)
         val totalCount = results.sumOf { it.count }
         println("$totalCount,$interestingCount,$forbiddenCount")
-//        if(forbiddenCount != 0L)
+        if(forbiddenCount != 0L)
             results.prettyPrint()
     }
 
