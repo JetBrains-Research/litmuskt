04f867c0c92e5e85af1a0a336198fc7439e589c5
Denis Lochmelis <dlochmelis@gmail.com>
2023-08-14 00:02:20 +0200

    Add JvmSpinBarrier, rename JvmBarrier to JvmCyclicBarrier

--- src/jvmMain/kotlin/komem/litmus/barriers/JvmCyclicBarrier.kt
+++ src/jvmMain/kotlin/komem/litmus/barriers/JvmCyclicBarrier.kt
@@ -2,7 +2,7 @@ package komem.litmus.barriers
 
 import java.util.concurrent.CyclicBarrier
 
-class JvmBarrier(threadCount: Int) : Barrier {
+class JvmCyclicBarrier(threadCount: Int) : Barrier {
     private val barrier = CyclicBarrier(threadCount)
 
     override fun await() {
