a8b0aa1a795014182fe437311f648f4a01d55858
Denis Lochmelis <dlochmelis@gmail.com>
2023-08-20 14:42:20 +0200

    Add checks for extra/missing outcome{} and spec{}

--- src/commonMain/kotlin/komem/litmus/LTDefinition.kt
+++ src/commonMain/kotlin/komem/litmus/LTDefinition.kt
@@ -14,21 +14,25 @@ class LTDefinitionScope<S>(
 ) {
     private val threadFunctions = mutableListOf<S.() -> Any?>()
     private lateinit var outcomeFinalizer: S.() -> LTOutcome
-    private var outcomeSpec = LTOutcomeSpecScope()
+    private lateinit var outcomeSpec: LTOutcomeSpecScope
 
     fun thread(function: S.() -> Unit) {
         threadFunctions.add(function)
     }
 
     fun outcome(function: S.() -> LTOutcome) {
+        if (::outcomeFinalizer.isInitialized) error("cannot set outcome more than once")
         outcomeFinalizer = function
     }
 
     fun spec(setup: LTOutcomeSpecScope.() -> Unit) {
-        outcomeSpec.setup()
+        if (::outcomeSpec.isInitialized) error("cannot set spec more than once")
+        outcomeSpec = LTOutcomeSpecScope().apply(setup)
     }
 
     fun build(): LTDefinition<S> {
+        if (threadFunctions.size < 2) error("tests require at least two threads")
+        if (!::outcomeSpec.isInitialized) error("spec not specified")
         val outcomeFinalizer: S.() -> LTOutcome = when {
             ::outcomeFinalizer.isInitialized -> outcomeFinalizer
             stateProducer() is AutoOutcome -> {
