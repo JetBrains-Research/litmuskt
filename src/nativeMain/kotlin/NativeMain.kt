4db197be2e89a11ee2586d9ce83bd39fd9ca86ef
Denis Lochmelis <dlochmelis@gmail.com>
2023-09-05 03:25:00 +0200

    Fix parameters

--- src/nativeMain/kotlin/NativeMain.kt
+++ src/nativeMain/kotlin/NativeMain.kt
@@ -9,15 +9,15 @@ fun main() {
     val test = VolatileAsFence
 
     val paramsList = variateRunParams(
-        batchSizeSchedule = listOf(100_000, 1_000_000),
+        batchSizeSchedule = listOf(1_000_000),
         affinityMapSchedule = listOf(null),
-        syncPeriodSchedule = generateSequence(5) { (it * 1.3).toInt() }.takeWhile { it < 1000 }.toList(),
+        syncPeriodSchedule = generateSequence(7) { (it * 1.5).toInt() }.takeWhile { it < 1000 }.toList(),
         barrierSchedule = listOf(::CinteropSpinBarrier)
     ).toList()
-    val timeLimit = 30.seconds
+    val timeLimit = 10.seconds
     println("params size: ${paramsList.size}")
     println("ETA: T+${(timeLimit * paramsList.size).toString(unit = DurationUnit.MINUTES)}")
     paramsList.map { params ->
-        runner.runTestParallel(30.seconds, params, test)
+        runner.runTestParallel(timeLimit, params, test)
     }.mergeResults().prettyPrint()
 }
