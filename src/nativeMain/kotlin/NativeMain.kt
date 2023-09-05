de041fc67f50335036ad637abde9bf52f070648f
Denis Lochmelis <dlochmelis@gmail.com>
2023-09-05 03:17:31 +0200

    Add test to native main()

--- src/nativeMain/kotlin/NativeMain.kt
+++ src/nativeMain/kotlin/NativeMain.kt
@@ -1,16 +1,23 @@
 import komem.litmus.*
 import komem.litmus.barriers.CinteropSpinBarrier
-import komem.litmus.testsuite.IRIWVolatile
+import komem.litmus.testsuite.VolatileAsFence
 import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit
 
 fun main() {
     val runner: LTRunner = WorkerRunner
-    val test = IRIWVolatile
-    val params = LTRunParams(
-        batchSize = 1_000_000,
-        syncPeriod = 10,
-        affinityMap = null,
-        barrierProducer = ::CinteropSpinBarrier
-    )
-    runner.runTest(30.seconds, params, test).prettyPrint()
+    val test = VolatileAsFence
+
+    val paramsList = variateRunParams(
+        batchSizeSchedule = listOf(100_000, 1_000_000),
+        affinityMapSchedule = listOf(null),
+        syncPeriodSchedule = generateSequence(5) { (it * 1.3).toInt() }.takeWhile { it < 1000 }.toList(),
+        barrierSchedule = listOf(::CinteropSpinBarrier)
+    ).toList()
+    val timeLimit = 30.seconds
+    println("params size: ${paramsList.size}")
+    println("ETA: T+${(timeLimit * paramsList.size).toString(unit = DurationUnit.MINUTES)}")
+    paramsList.map { params ->
+        runner.runTestParallel(30.seconds, params, test)
+    }.mergeResults().prettyPrint()
 }
