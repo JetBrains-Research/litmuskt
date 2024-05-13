b463394690a9832a7a4e0afeb7fa36e8ce4f7c7b
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-05-13 16:52:22 +0200

    move MPNoDRF into `MessagePassing.kt`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- testsuite/src/commonMain/kotlin/org/jetbrains/litmuskt/tests/MessagePassing.kt
+++ testsuite/src/commonMain/kotlin/org/jetbrains/litmuskt/tests/MessagePassing.kt
@@ -73,4 +73,24 @@ object MessagePassing {
         }
     }
 
+    val MissingVolatile = litmusTest({
+        object : LitmusIOutcome() {
+            var x = 0
+            var y = 0
+        }
+    }) {
+        thread {
+            x = 1
+            y = 1
+        }
+        thread {
+            r1 = if (y != 0) x else -1
+        }
+        spec {
+            accept(1)
+            accept(-1)
+            interesting(0)
+        }
+    }
+
 }
