30d25b3cd5f37f832e49ffece1eaf8eb6b2209ac
Denis Lochmelis <dlochmelis@gmail.com>
2024-05-22 14:46:52 +0200

    Abstract pthread away into cinterop to support Macos

--- core/src/nativeMain/kotlin/org/jetbrains/litmuskt/PthreadRunner.kt
+++ core/src/nativeMain/kotlin/org/jetbrains/litmuskt/PthreadRunner.kt
@@ -1,10 +1,10 @@
 package org.jetbrains.litmuskt
 
 import kotlinx.cinterop.*
-import platform.posix.errno
-import platform.posix.pthread_create
-import platform.posix.pthread_join
-import platform.posix.strerror
+import kpthread.k_pthread_create
+import kpthread.k_pthread_join
+import kpthread.k_pthread_t_alloc
+import kpthread.k_pthread_t_free
 
 private class ThreadData<S : Any>(
     val states: Array<S>,
@@ -39,42 +39,55 @@ class PthreadRunner : LitmusRunner() {
     ): () -> LitmusResult {
         val barrier = barrierProducer(test.threadCount)
 
-        fun startThread(threadIndex: Int): Pair<PthreadVar, StableRef<*>> {
+        fun startThread(threadIndex: Int): Pair<COpaquePointer, StableRef<*>> {
             val function: (Any) -> Unit = { state ->
                 @Suppress("UNCHECKED_CAST")
                 test.threadFunctions[threadIndex].invoke(state as S)
             }
             val threadData = ThreadData(states, function, syncPeriod, barrier)
-
             val threadDataRef = StableRef.create(threadData)
-            val pthreadVar = nativeHeap.alloc<PthreadVar>()
-            val code = pthread_create(
-                __newthread = pthreadVar.ptr,
-                __attr = null,
-                __start_routine = staticCFunction<COpaquePointer?, COpaquePointer?> {
+
+//            val pthreadVar = nativeHeap.alloc<PthreadVar>()
+            val pthreadPtr = k_pthread_t_alloc() ?: error("could not allocate pthread_t pointer")
+
+//            val code = pthread_create(
+//                __newthread = pthreadPtr,
+//                __attr = null,
+//                __start_routine = staticCFunction<COpaquePointer?, COpaquePointer?> {
+//                    val data = it!!.asStableRef<ThreadData<S>>().get()
+//                    threadRoutine(data)
+//                    return@staticCFunction null
+//                },
+//                __arg = threadDataRef.asCPointer(),
+//            )
+//            if (code != 0) error("pthread_create failed; errno means: ${strerror(errno)?.toKString()}")
+            k_pthread_create(
+                pthreadPtr,
+                staticCFunction<COpaquePointer?, COpaquePointer?> {
                     val data = it!!.asStableRef<ThreadData<S>>().get()
                     threadRoutine(data)
                     return@staticCFunction null
                 },
-                __arg = threadDataRef.asCPointer(),
-            )
-            if (code != 0) error("pthread_create failed; errno means: ${strerror(errno)?.toKString()}")
+                threadDataRef.asCPointer()
+            ).syscallCheck()
+
             // TODO: I don't think there is a way to assign affinity before the thread starts (would be useful for MacOS)
-            org.jetbrains.litmuskt.getAffinityManager()?.let { am ->
-                val map = affinityMap?.allowedCores(threadIndex) ?: return@let
-                am.setAffinity(pthreadVar.value, map)
-                require(am.getAffinity(pthreadVar.value) == map) { "setting affinity failed" }
-            }
-            return pthreadVar to threadDataRef
+//            getAffinityManager()?.let { am ->
+//                val map = affinityMap?.allowedCores(threadIndex) ?: return@let
+//                am.setAffinity(pthreadVar.value, map)
+//                require(am.getAffinity(pthreadVar.value) == map) { "setting affinity failed" }
+//            }
+            return pthreadPtr to threadDataRef
         }
 
         val threads = List(test.threadCount) { startThread(it) }
 
         return {
-            for ((pthreadVar, threadDataRef) in threads) {
-                pthread_join(pthreadVar.value, null).syscallCheck()
-
-                nativeHeap.free(pthreadVar)
+            for ((pthreadPtr, threadDataRef) in threads) {
+//                pthread_join(pthreadVar.value, null).syscallCheck()
+//                nativeHeap.free(pthreadVar)
+                k_pthread_join(pthreadPtr, null).syscallCheck()
+                k_pthread_t_free(pthreadPtr)
                 threadDataRef.dispose()
             }
             calcStats(states, test.outcomeSpec, test.outcomeFinalizer)
