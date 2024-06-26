672b98460a811b62596f6c321721ae0669929ace
Denis Lochmelis <dlochmelis@gmail.com>
2024-06-26 17:10:52 +0200

    Return affinity support for pthread

--- core/src/linuxMain/kotlin/org/jetbrains/litmuskt/AffinityBindingsImplPosix.kt
+++ core/src/linuxMain/kotlin/org/jetbrains/litmuskt/AffinityBindingsImplPosix.kt
@@ -1,45 +1,48 @@
 package org.jetbrains.litmuskt
 
 import kaffinity.*
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.alloc
-import kotlinx.cinterop.memScoped
-import kotlinx.cinterop.ptr
+import kotlinx.cinterop.*
 import platform.posix.cpu_set_t
 import platform.posix.pthread_t
 import kotlin.native.concurrent.ObsoleteWorkersApi
 
-@OptIn(ExperimentalStdlibApi::class, ObsoleteWorkersApi::class)
+@OptIn(ExperimentalStdlibApi::class, ObsoleteWorkersApi::class, ExperimentalForeignApi::class)
 actual val affinityManager: AffinityManager? = object : AffinityManager {
-
-    // TODO: affinity for k_pthread
 
     override fun setAffinity(threadlike: Threadlike, cpus: Set<Int>): Boolean {
         when (threadlike) {
-            is WorkerThreadlike -> setPthreadDirectAffinity(threadlike.worker.platformThreadId, cpus)
+            is WorkerThreadlike -> memScoped {
+                val pthreadPtr = alloc<pthread_t>(threadlike.worker.platformThreadId).ptr
+                setPthreadAffinity(pthreadPtr, cpus)
+            }
+            is PthreadThreadlike -> setPthreadAffinity(threadlike.pthreadPtr, cpus)
             else -> return false
         }
         return true
     }
 
     override fun getAffinity(threadlike: Threadlike): Set<Int>? = when (threadlike) {
-        is WorkerThreadlike -> getPthreadDirectAffinity(threadlike.worker.platformThreadId)
+        is WorkerThreadlike -> memScoped {
+            val pthreadPtr = alloc<pthread_t>(threadlike.worker.platformThreadId).ptr
+            getPthreadAffinity(pthreadPtr)
+        }
+        is PthreadThreadlike -> getPthreadAffinity(threadlike.pthreadPtr)
         else -> null
     }
 
     @OptIn(ExperimentalForeignApi::class)
-    private fun setPthreadDirectAffinity(thread: pthread_t, cpus: Set<Int>): Unit = memScoped {
+    private fun setPthreadAffinity(threadPtr: CPointer<*>, cpus: Set<Int>): Unit = memScoped {
         require(cpus.isNotEmpty())
         val set = alloc<cpu_set_t>()
         cpu_zero(set.ptr)
         for (cpu in cpus) cpu_set(cpu, set.ptr)
-        set_affinity(thread, set.ptr).syscallCheck()
+        set_affinity(threadPtr, set.ptr).syscallCheck()
     }
 
     @OptIn(ExperimentalForeignApi::class)
-    private fun getPthreadDirectAffinity(thread: pthread_t): Set<Int> = memScoped {
+    private fun getPthreadAffinity(threadPtr: CPointer<*>): Set<Int> = memScoped {
         val set = alloc<cpu_set_t>()
-        get_affinity(thread, set.ptr).syscallCheck()
+        get_affinity(threadPtr, set.ptr).syscallCheck()
         return (0..<cpu_setsize())
             .filter { cpu_isset(it, set.ptr) != 0 }
             .toSet()
