4f771d71acd90a130aef71e1aa06f4353e164bc3
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2025-01-16 16:48:04 +0100

    Upgrade to Kotlin 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "2.0.0" apply false
+    kotlin("multiplatform") version "2.1.0" apply false
 }
 
 repositories {
--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    implementation("com.google.devtools.ksp:symbol-processing-api:2.0.0-1.0.23")
+    implementation("com.google.devtools.ksp:symbol-processing-api:2.1.0-1.0.29")
 }
